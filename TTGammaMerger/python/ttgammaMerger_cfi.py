a07af20bacfe56855647522ecc85d25541ad4462
htholen <>
2013-07-19 11:44:36 +0000

    fix: parameter not there anymore

--- TTGammaMerger/python/ttgammaMerger_cfi.py
+++ TTGammaMerger/python/ttgammaMerger_cfi.py
@@ -4,7 +4,6 @@ import FWCore.ParameterSet.Config as cms
 ttbarPhotonMerger2to5 = cms.EDFilter("TTGammaMerger",
     ptCut = cms.double(20.),
     drCut = cms.double(.1),
-    is2to5 = cms.untracked.bool(True),
     filter = cms.bool(True),
 )
 
