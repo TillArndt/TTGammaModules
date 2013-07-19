69bf2c8f563e44c41448f29fd29656409640c24d
htholen <>
2013-07-19 11:40:46 +0000

    removing dependence on ttGenEvent from python config

--- TTGammaMerger/python/ttgammaMerger_cfi.py
+++ TTGammaMerger/python/ttgammaMerger_cfi.py
@@ -1,15 +1,5 @@
 
 import FWCore.ParameterSet.Config as cms
-from TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff import *
-
-
-ttbarPhotonMerger2to7 = cms.EDFilter("TTGammaMerger",
-    ptCut = cms.double(8.),
-    drCut = cms.double(.1),
-    is2to7 = cms.untracked.bool(True),
-    filter = cms.bool(True),
-)
-ttgammaMerging2to7 = cms.Sequence(makeGenEvt * ttbarPhotonMerger2to7)
 
 ttbarPhotonMerger2to5 = cms.EDFilter("TTGammaMerger",
     ptCut = cms.double(20.),
@@ -17,7 +7,6 @@ ttbarPhotonMerger2to5 = cms.EDFilter("TTGammaMerger",
     is2to5 = cms.untracked.bool(True),
     filter = cms.bool(True),
 )
-ttgammaMerging2to5 = cms.Sequence(makeGenEvt * ttbarPhotonMerger2to5)
 
 
 
